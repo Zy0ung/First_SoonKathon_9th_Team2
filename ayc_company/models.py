723b657a45277fe1391379f177c6c3739f5b288e
YebinLeee <lyb2325@gmail.com>
2021-07-02 22:17:38 +0900

    [이예빈][세팅] :: model (Jean) 생성

--- ayc_company/models.py
+++ ayc_company/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Jean(models.Model): # models 모듈 안에 있는 Model 클래스 상속
+    name=models.CharField(max_length=100)
+    price=models.CharField(max_length=100)
+    material=models.TextField()
+    detail=models.TextField()
+    image=models.ImageField(upload_to = "anc_company/", blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
