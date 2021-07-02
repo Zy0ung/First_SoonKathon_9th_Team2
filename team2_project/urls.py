2d14c73b76b3f1c2129be9d12977a68062cede10
Jiyoung <thdwldud8@gmail.com>
2021-07-02 21:00:50 +0900

    [송지영][세팅] :: urls 재세팅

--- team2_project/urls.py
+++ team2_project/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from ayc_company import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',ayc_company.views.index, name='index'),
+    path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
