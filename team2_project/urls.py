08114ad15a8511f1b6c59631dc6f1374060f8a2c
mslee5240 <ms.lee5240@gmail.com>
2021-07-02 20:24:14 +0900

    [이민수][세팅]::최종 세팅 완료

--- team2_project/urls.py
+++ team2_project/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',ayc_company.views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
